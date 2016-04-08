4c7469ca1ef31b8bd5da79cfbf38a2648e3f2862
yanglw <weigeku8@gmail.com>
2016-04-09 00:27:30 +0800

    设置 AllMessageHandler 输出接收到所有的 Socket 信息。

--- src/main/java/com/github/yanglw/douyu/message/handler/AllMessageHandler.java
+++ src/main/java/com/github/yanglw/douyu/message/handler/AllMessageHandler.java
@@ -1,10 +1,19 @@
 package com.github.yanglw.douyu.message.handler;
 
+import com.github.yanglw.douyu.message.Message;
+import com.github.yanglw.douyu.socket.Connector;
+import com.github.yanglw.douyu.util.LogUtils;
+
 /**
  * Created by yanglw on 2016-4-8.
  */
-public abstract class AllMessageHandler extends TargetTypeMessageHandler {
+public class AllMessageHandler extends TargetTypeMessageHandler {
     public AllMessageHandler() {
         super(null);
+    }
+
+    @Override
+    public void handleMessage(Connector connector, Message message) {
+        LogUtils.printf(message.getMsg());
     }
 }
